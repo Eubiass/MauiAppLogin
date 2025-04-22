1b5f26e7f853d036fd29aae6cb2805ae2fa6afc9
Aalvebia <aalvebia03@gmail.com>
2025-04-22 11:35:03 -0300

    Finalização do projeto Login.

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -6,7 +6,24 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            String? usuario_logado = null;
+
+            Task.Run(async () =>
+            {
+                usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+                
+                if (usuario_logado == null)
+                {
+                    MainPage = new Login();
+                }else
+                {
+                    MainPage = new Protegida();
+                }
+            
+            
+            });
+
+            MainPage = new Login();
         }
 
         protected override Window CreateWindow(IActivationState activationState)
@@ -14,7 +31,7 @@
             var window = base.CreateWindow(activationState);
 
             window.Width = 400;
-            window.Height = 700;
+            window.Height = 600;
 
             return window;
         }
